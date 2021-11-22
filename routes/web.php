875cc5e6a711db97eace5a7114c7085c0717ab89
Sergej <vasiliokas@gmail.com>
2021-11-22 19:45:20 +0000

    Admin only route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/users', function () {
+    return view('users');
+})->middleware(['auth', 'roleChecker:admin, null'])->name('users');
+
 require __DIR__.'/auth.php';
