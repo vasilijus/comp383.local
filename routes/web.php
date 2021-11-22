3c4f044941da10018257a8d8f72727738a7cad30
Sergej <vasiliokas@gmail.com>
2021-11-22 23:51:24 +0000

    route update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +20,11 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard')->with('user', Auth::user());
 })->middleware(['auth'])->name('dashboard');
 
 Route::get('/users', function () {
-    return view('users');
+    return view('users')->with('users', User::all());
 })->middleware(['auth', 'roleChecker:admin, null'])->name('users');
 
 require __DIR__.'/auth.php';
